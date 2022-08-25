dfaf4a7fc211bb16fb78cac355c5aa35fb386a05
mahakagarwal248 <mahak.agarwal@techchefz.com>
2022-08-25 11:43:55 +0530

    holiday calendar save to database done

--- controllers/holidayController.js
+++ controllers/holidayController.js
@@ -1,8 +1,9 @@
 import csv from 'csvtojson'
+import moment from 'moment'
 
-import holidayCalendar from "../models/holidayCalendarSchema.js";
+import holidayCalendar from '../models/holidayCalendarSchema.js'
 
-export const addHolidayCalendar = async (req,res) => {
+export const addHolidayCalendar = async (req, res) => {
     const filePath = req.file.path
     try {
         const holidayDetails = await csv()
@@ -10,8 +11,30 @@ export const addHolidayCalendar = async (req,res) => {
             .then((jsonObj) => {
                 return jsonObj
             })
-        console.log(holidayDetails)
+
+        let newArray = []
+        for (var y in holidayDetails) {
+            const obj = holidayDetails[y]
+            newArray.push({
+                date: obj.date,
+                occasion: obj.occasion,
+                typeOfHoliday: obj.typeOfHoliday,
+            })
+        }
+        // console.log(newArray)
+        const newList = {
+            holidays:newArray,
+            createdBy: holidayDetails[0].createdBy,
+            calendarName: holidayDetails[0].calendarName,
+        }
+        // console.log(newList)
+        const holidays = await holidayCalendar.create(
+            newList,
+        )
+        holidays.save()
+
+        res.send('success')
     } catch (error) {
-        console.log(error);
+        console.log(error)
     }
 }
--- models/holidayCalendarSchema.js
+++ models/holidayCalendarSchema.js
@@ -1,13 +1,7 @@
 import mongoose from 'mongoose'
 
 const holidayCalendarSchema = mongoose.Schema({
-    holidays: [
-        {
-            date: { type: Date, required: true },
-            occasion: { type: String, required: true },
-            typeOfHoliday: { type: String, required: true },
-        },
-    ],
+    holidays: { type: Array, default: [] },
     createdBy: { type: String, required: true },
     companyId: { type: mongoose.Schema.Types.ObjectId, ref: 'companies' },
     calendarName: { type: String },
