acf4380d1d9437990c87620b3258c085049c73af
mahakagarwal248 <mahak.agarwal@techchefz.com>
2022-08-25 10:24:31 +0530

    holiday calendar route created

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-import express from "express";
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
 
-import companiesRoutes from './routes/companiesRoutes.js';
-import userRoutes from './routes/userRoutes.js';
+import companiesRoutes from './routes/companiesRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import holidayRoutes from './routes/holidayRoutes.js'
 
-const app = express();
-dotenv.config();
-app.use(express.json({limit:"30mb" , extended:true}))
-app.use(express.urlencoded({limit:"30mb" , extended:true}))
-app.use(cors());
+const app = express()
+dotenv.config()
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
 
-app.get('/',(req,res)=>{
-    res.send("This is a shopping site API");
+app.get('/', (req, res) => {
+    res.send('This is a shopping site API')
 })
 
-app.use("/companies", companiesRoutes);
-app.use("/user", userRoutes);
+app.use('/companies', companiesRoutes)
+app.use('/user', userRoutes)
+app.use('/holiday', holidayRoutes)
 
-const PORT = process.env.PORT || 5000;
-const DB_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000
+const DB_URL = process.env.CONNECTION_URL
 
-mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
-    .catch((err) => console.log(err.message));
+mongoose
+    .connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(
+        app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+    )
+    .catch((err) => console.log(err.message))
---
+++ routes/holidayRoutes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import multer from 'multer';
+
+import {addHolidayCalendar} from '../controllers/holidayController.js';
+
+const router = express.Router()
+const storage = multer.diskStorage({
+    destination:"/files"
+})
+const app = express();
+
+var upload = multer({ storage: storage })
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+router.post('/addCalendar', upload.single('file'), addHolidayCalendar)
+
+export default router;
