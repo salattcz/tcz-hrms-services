62c8ee68d4da2e687c4e01031af8c9b39025a84a
mahakagarwal248 <mahak.agarwal@techchefz.com>
2022-09-07 11:42:40 +0530

    files formatted

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-import express from "express";
+import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
-import companiesRoutes from './routes/companiesRoutes.js'
+import companiesRoutes from './routes/companiesRoutes.js';
 const app = express();
 dotenv.config();
-app.use(express.json({limit:"30mb" , extended:true}))
-app.use(express.urlencoded({limit:"30mb" , extended:true}))
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-app.get('/',(req,res)=>{
-    res.send("This is a shopping site API");
-})
+app.get('/', (req, res) => {
+    res.send('This is a shopping site API');
+});
 
-app.use("/companies", companiesRoutes);
+app.use('/companies', companiesRoutes);
 const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.CONNECTION_URL;
 
-mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
+mongoose
+    .connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(
+        app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+    )
     .catch((err) => console.log(err.message));
