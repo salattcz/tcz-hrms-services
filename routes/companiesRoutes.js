56a0ecd9db9240ca82424eee75567a5cf01e2450
mahakagarwal248 <mahak.agarwal@techchefz.com>
2022-09-07 11:34:10 +0530

    files formatted

--- routes/companiesRoutes.js
+++ routes/companiesRoutes.js
@@ -1,14 +1,14 @@
-import express from 'express'
+import express from 'express';
 import {
     register,
     generateNewToken,
     login,
-} from '../controllers/companiesController.js'
+} from '../controllers/companiesController.js';
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/register', register)
-router.post('/generate-token', generateNewToken)
-router.post('/login', login)
+router.post('/register', register);
+router.post('/generate-token', generateNewToken);
+router.post('/login', login);
 
-export default router
+export default router;
